1204d4d9500c2197bfe2da3aa032a4fd4a4742cf
Kevin Arindaeng <karindaeng@gmail.com>
2017-12-19 22:16:11 -0500

    finished ability scores and modifiers

--- one_shot_generator.py
+++ one_shot_generator.py
@@ -7,10 +7,45 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     Strengthscore = randint(3,18)
+    Dexterityscore = randint(3,18)
+    Constitutionscore = randint(3,18)
+    Wisdomscore = randint(3,18)
+    Intelligencescore = randint(3,18)
+    Charismascore = randint(3,18)
     return render_template('index.html',
         charname="Kevin",
         Strengthscore = Strengthscore,
-        Strengthmod = randint(1,10))
+        Dexterityscore = Dexterityscore,
+        Constitutionscore = Constitutionscore,
+        Wisdomscore = Wisdomscore,
+        Intelligencescore = Intelligencescore,
+        Charismascore = Charismascore,
+        Strengthmod=get_ability_modifier(Strengthscore),
+        Dexteritymod = get_ability_modifier(Dexterityscore),
+        Constitutionmod = get_ability_modifier(Constitutionscore),
+        Wisdommod = get_ability_modifier(Wisdomscore),
+        Intelligencemod = get_ability_modifier(Intelligencescore),
+        Charismamod = get_ability_modifier(Charismascore))
+
+def get_ability_modifier(mod):
+    return {
+        3: '-4',
+        4: '-3',
+        5: '-3',
+        6: '-2',
+        7: '-2',
+        8: '-1',
+        9: '-1',
+        10: '0',
+        11: '0',
+        12: '+1',
+        13: '+1',
+        14: '+2',
+        15: '+2',
+        16: '+3',
+        17: '+3',
+        18: '+4'
+    }.get(mod, '0')
 
 if __name__ == '__main__':
     app.run()
