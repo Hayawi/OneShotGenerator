0631ae32edaad20a721f47e828b2cb1ec664abf5
Kevin Arindaeng <karindaeng@gmail.com>
2017-12-19 21:36:35 -0500

    Added strength score RNG

--- one_shot_generator.py
+++ one_shot_generator.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
+from random import randint
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
-
+def main():
+    Strengthscore = randint(3,18)
+    return render_template('index.html',
+        charname="Kevin",
+        Strengthscore = Strengthscore,
+        Strengthmod = randint(1,10))
 
 if __name__ == '__main__':
     app.run()
