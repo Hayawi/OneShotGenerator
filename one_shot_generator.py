ad85f929f830fad2523d13bcbb1128a1532b429a
Yahya <yahya.ismail@hotmail.ca>
2017-12-20 20:21:47 -0500

    Added the dungeon generator

--- one_shot_generator.py
+++ one_shot_generator.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from random import randint
 
 app = Flask(__name__)
-
+app.debug = 'true'
 
 @app.route('/')
 def main():
@@ -51,6 +51,47 @@ def get_ability_modifier(mod):
         17: '+3',
         18: '+4'
     }.get(mod, '0')
+
+@app.route('/dungeon')
+def dungeon_generator():
+	dungeonRow = 50
+	dungeonCol = 100
+	dungeonMap = [[1 for i in range(dungeonCol)] for j in range(dungeonRow)]
+	initializationPass = 1
+	carvingPass = 9
+	for i in range(initializationPass):
+		for j in range (dungeonRow):
+			for k in range (dungeonCol):
+				if (randint(1, 100) == 1):
+					dungeonMap[j][k] = 0
+					
+	for i in range(carvingPass):
+		for j in range(dungeonRow):
+			for k in range(dungeonCol):
+				
+				if (dungeonMap[j][k] == 1 and j > 0 and j < dungeonRow - 1 and k > 0 and k < dungeonCol - 1):
+					if ((dungeonMap[j-1][k] == 0 and dungeonMap[j+1][k] == 0) or (dungeonMap[j][k-1] == 0 and dungeonMap[j][k+1] == 0)):
+						dungeonMap[j][k] = 0;
+						
+				if (dungeonMap[j][k] == 0):
+					xChange = randint(-1,1)
+					yChange = randint(-1,1)
+					if (j+xChange > 0 and j+xChange < dungeonRow and k+yChange > 0 and k +yChange < dungeonCol):
+						dungeonMap[j+xChange][k+yChange] = 0					
+	
+	dungeonLayout = dungeofy(dungeonMap, dungeonRow, dungeonCol)
+	return render_template('home.html', dungeon=dungeonLayout)
+	
+def dungeofy(map, dungeonRow, dungeonCol):
+	dungeonLayout = [[" " for i in range(dungeonCol)] for j in range(dungeonRow)]
+	dungeonString = ''
+	for i in range(dungeonRow):
+		for j in range(dungeonCol):
+			if (map[i][j] == 1):
+				dungeonLayout[i][j] = "#"
+			dungeonString += dungeonLayout[i][j]
+		dungeonString += '\n'
+	return dungeonString
 
 if __name__ == '__main__':
     app.run()
